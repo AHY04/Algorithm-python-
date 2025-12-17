fc3ec9b97bcee461747c330ff9cf47d4a8bbbced
HyunYong <169076243+AHY04@users.noreply.github.com>
2025-12-17 18:30:30 +0900

    Add function to remove leading zeros from string
    Implement solution function to strip leading zeros from a string.

---
+++ 난이도0/0떼기/코드.py
@@ -0,0 +1,6 @@
+def solution(n_str):
+    return n_str.lstrip('0')
+
+## strip(문자): 인자로 전달된 문자를 String의 왼쪽과 오른쪽에서 제거
+## lstrip(문자): 인자로 전달된 문자를 String의 왼쪽에서 제거
+## rstrip(문자): 인자로 전달된 문자를 String의 오른쪽에서 제거(인자가 없다면 공백을 제거)
